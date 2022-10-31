02a8331967debae63a03ee7e07edfcf22794231f
Sina Kazi <Dr.TinTin92@gmail.com>
2022-11-01 00:38:13 +0330

    creat style.css and add font farsi and change english var to farsi to views page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="stylesheet" href="src/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Vazirmatn"/>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
